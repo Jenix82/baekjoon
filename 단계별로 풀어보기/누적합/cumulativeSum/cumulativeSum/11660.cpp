b759bcd7d87e1553b4ecab250afb732a765ce4c7
Jenix82 <cjlhs0691@naver.com>
2023-02-23 02:24:57 +0900

    230223-02:24

--- 단계별로 풀어보기/누적합/cumulativeSum/cumulativeSum/11660.cpp
+++ 단계별로 풀어보기/누적합/cumulativeSum/cumulativeSum/11660.cpp
@@ -3,30 +3,28 @@
 #include <vector>
 using namespace std;
 
-// ¾ÆÁ÷ ¸ø Ç¯
+// Á» ¸ÚÀÖ°Ô Ç¬ µí?
 
 void p11660()
 {
 	int N, M;
 	scanf("%d %d", &N, &M);
-	vector<vector<int>> seq(N, vector<int>(N, 0));
-	vector<int> sum(N * N, 0);
+	vector<vector<int>> seq(N+1, vector<int>(N+1, 0));
+	vector<vector<int>> sum(N+1, vector<int>(N+1, 0));
 	vector<int> ans(M, 0);
 
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
+	for (int i = 1; i <= N; i++)
+		for (int j = 1; j <= N; j++)
 		{
 			scanf("%d", &seq[i][j]);
-			if (i + j == 0) sum[i * N + j] = seq[i][j];
-			else sum[i * N + j] = sum[i * N + j - 1] + seq[i][j];
+			sum[i][j] = sum[i - 1][j] + sum[i][j - 1] + seq[i][j] - sum[i - 1][j - 1];
 		}
 
 	for (int i = 0; i < M; i++)
 	{
 		int x1, y1, x2, y2;
 		scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
-		if (x1 * y1 == 1) ans[i] = sum[(x2 - 1) * N + (y2 - 1)];
-		else ans[i] = sum[(x2 - 1) * N + (y2 - 1)] - sum[(x1 - 1) * N + (y1 - 1) - 1];
+		ans[i] = sum[x2][y2] - sum[x1 - 1][y2] - sum[x2][y1 - 1] + sum[x1 - 1][y1 - 1];
 	}
 
 	for (auto& a : ans) printf("%d\n", a);
