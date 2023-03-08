ba1a28b5581c2136f57201cba487620801506462
Jenix82 <cjlhs0691@naver.com>
2023-03-09 02:29:50 +0900

    230309-02:29

--- 심화문제/27526/27526/main.cpp
+++ 심화문제/27526/27526/main.cpp
@@ -1,47 +1,48 @@
 #define _CRT_SECURE_NO_WARNINGS 
 #include <iostream>
-#include <vector>
+#include <vector> 
 #include <utility>
-#include <map>
+#include <algorithm>
 using namespace std;
-
-int N, M;
-vector<vector<int>> slide;
-vector<int*> ptr;
-
-inline void run(int i)
-{
-	int n = N * M;
-	if (i % n == 0)
-	{
-		if ((i / n) % 2 == 0) swap(*ptr[0], *ptr[1]);
-		else swap(*ptr[n - 1], *ptr[n - 2]);
-	}
-	else
-	{
-		int r = i % n;
-		if ((i / n) % 2 == 0) swap(*ptr[n - r], *ptr[n - r - 1]);
-		else swap(*ptr[r], *ptr[r - 1]);
-	}
-}
 
 int main()
 {
+	int N, M, count = 1;
+	vector<vector<int>> slide;
+	vector<int*> ptr;
+
 	scanf("%d %d", &N, &M);
 	slide.resize(N, vector<int>(M, 0));
 	ptr.resize(N * M, nullptr);
-	map<vector<vector<int>>, int> check;
-
-	for (int i = 1; i <= N; i++)
-		for (int j = 1; j <= M; j++)
-			slide[i-1][j-1] = (i - 1) * M + j;
 
 	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+			slide[i][j] = i * M + j + 1;
 		for (int j = 0; j < M; j++)
 			ptr[i * M + j] = (i % 2 == 0) ? &slide[i][j] : &slide[i][M - 1 - j];
+	}	
 
-	int count = 1;
 	for (int i = 1; i <= N * M; i++) count *= i;
+
+	vector<vector<int>> save(max(4, N * M + 1), vector<int>(0, 0));
+	save[2].emplace_back(0);
+	save[3].emplace_back(1);
+	save[3].emplace_back(0);
+
+	for (int i = 4; i <= N * M; i++)
+	{
+		int idx = 0;
+		save[i].resize(save[i - 1].size() * i, 0);
+
+		for (int j = 0; j < save[i - 1].size();)
+		{
+			for (int k = i - 2; k >= 0; k--) save[i][idx++] = k;
+			save[i][idx++] = save[i - 1][j++] + 1;
+			for (int k = 0; k <= i - 2; k++) save[i][idx++] = k;
+			save[i][idx++] = save[i - 1][j++];
+		}
+	}
 
 	for (int i = 0; i < count; i++)
 	{
@@ -52,8 +53,8 @@ int main()
 				else printf("%d", slide[i][j]);
 			printf("\n");
 		}
-		check[slide]++;
-		run(i);
+
+		int cur = save[N * M][i % save[N * M].size()];
+		swap(*ptr[cur], *ptr[cur++]);
 	}
-	printf("%d / %zd\n", count, check.size());
 }
