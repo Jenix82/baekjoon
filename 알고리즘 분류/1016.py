f8b535889bc3153ec5040e8192aaa28704e826bb
Jenix82 <cjlhs0691@naver.com>
2023-03-16 01:51:15 +0900

    230316-01:51

---
+++ 알고리즘 분류/1016.py
@@ -0,0 +1,24 @@
+import sys
+import numpy as np
+import math
+
+m, M = map(int, sys.stdin.readline().rstrip().split())
+List = np.ones((M - m + 1, ), dtype = int)
+i = 2
+while i * i <= M:
+    if i > 2 and i % 2 == 0 :
+        i += 1
+        continue
+    
+    left, right = math.ceil(m / (i * i)), math.floor(M / (i * i))
+    if (i == 23):
+        print(left, right)
+        print(right * i * i - m)
+    tmp = ((np.arange(right - left + 1) + left) * (i * i)) - m
+    if (i == 23): print(tmp)
+    np.put(List, list(tmp), 0)
+    i += 1
+
+print(np.count_nonzero(List))
+
+    
